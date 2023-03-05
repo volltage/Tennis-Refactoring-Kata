d61ae70c7bcdc63aee43c28e8b248561e7ca6654
Adrian Misztal <gryf06@gmail.com>
2023-03-05 21:41:14 +0100

    refactor of TennisGame1.kt

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -1,51 +1,44 @@
 class TennisGame1(private val player1Name: String, private val player2Name: String) : TennisGame {
 
-    private var m_score1: Int = 0
-    private var m_score2: Int = 0
+    private var mScore1: Int = 0
+    private var mScore2: Int = 0
 
     override fun wonPoint(playerName: String) {
         if (playerName === "player1")
-            m_score1 += 1
+            mScore1 += 1
         else
-            m_score2 += 1
+            mScore2 += 1
     }
 
-    override fun getScore(): String {
-        var score = ""
-        var tempScore = 0
-        if (m_score1 == m_score2) {
-            when (m_score1) {
-                0 -> score = "Love-All"
-                1 -> score = "Fifteen-All"
-                2 -> score = "Thirty-All"
-                else -> score = "Deuce"
-            }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            val minusResult = m_score1 - m_score2
-            if (minusResult == 1)
-                score = "Advantage player1"
-            else if (minusResult == -1)
-                score = "Advantage player2"
-            else if (minusResult >= 2)
-                score = "Win for player1"
-            else
-                score = "Win for player2"
-        } else {
-            for (i in 1..2) {
-                if (i == 1)
-                    tempScore = m_score1
-                else {
-                    score += "-"
-                    tempScore = m_score2
-                }
-                when (tempScore) {
-                    0 -> score += "Love"
-                    1 -> score += "Fifteen"
-                    2 -> score += "Thirty"
-                    3 -> score += "Forty"
-                }
-            }
+    override fun getScore() =
+        when {
+            mScore1 == mScore2 -> generateDeuceScore()
+            mScore1 >= 4 || mScore2 >= 4 -> generateWinScore()
+            else -> generateCurrentResult()
         }
-        return score
+
+    private fun generateCurrentResult() = listOf(mScore1, mScore2)
+        .map {
+            when (it) {
+                0 -> "Love"
+                1 -> "Fifteen"
+                2 -> "Thirty"
+                3 -> "Forty"
+                else -> ""
+            }
+        }.joinToString("-")
+
+    private fun generateWinScore() = when {
+        mScore1 - mScore2 == 1 -> "Advantage player1"
+        mScore1 - mScore2 == -1 -> "Advantage player2"
+        mScore1 - mScore2 >= 2 -> "Win for player1"
+        else -> "Win for player2"
+    }
+
+    private fun generateDeuceScore() = when (mScore1) {
+        0 -> "Love-All"
+        1 -> "Fifteen-All"
+        2 -> "Thirty-All"
+        else -> "Deuce"
     }
 }
